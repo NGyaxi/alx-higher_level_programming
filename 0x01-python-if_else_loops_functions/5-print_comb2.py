6476f1abda5c3585c91b4c2516c95dd1e2502780
root <fredrickgyasi@gmail.com>
2023-10-12 00:56:42 -0700

    if else statement

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i < 99:
-        print(f'{i:02d}, ', end='')
+for number in range(100):
+    if number < 99:
+        print(f"{number:02d}, ", end='')
     else:
-        print(f'{i:02d}')
+        print(f"{number:02d}", end='\n')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for first_digit in range(10):
     for second_digit in range(first_digit + 1, 10):
-        if first_digit < 9 or second_digit < 9:
-            print(f'{first_digit}{second_digit}, ', end='')
+        if first_digit < 9:
+            print("{:02d}, {:02d}, ".format(first_digit, second_digit), end='')
         else:
-            print(f'{first_digit}{second_digit}')
-
-print()
+            print("{:02d}, {:02d}".format(first_digit, second_digit), end='\n')
