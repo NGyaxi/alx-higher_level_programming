1fd4866f7a9b6c7ccf9f6bbb82b94f933332c0b9
root <fredrickgyasi@gmail.com>
2023-10-14 23:00:01 -0700

    python structures

--- 0x03-python-data_structures/list.h
+++ 0x03-python-data_structures/list.h
@@ -1,6 +1,27 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
-void print_list_integer(int my_list[], int list_length);
+#include <stdio.h>
 
-#endif
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+
+int is_palindrome(listint_t **head);
+int palindrome_check(listint_t *tmp_head, int count);
+
+#endif /* LISTS_H */
