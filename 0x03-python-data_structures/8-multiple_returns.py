d26b0198c9bcb82d0ee14300692e1769fa3ce612
root <fredrickgyasi@gmail.com>
2023-10-15 02:46:39 -0700

    py3

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     count = len(sentence)
-    if (sentence == ""):
+    if (sentence[0] == 0):
     	print("none")
     else:
     	print(count,sentence[0])
